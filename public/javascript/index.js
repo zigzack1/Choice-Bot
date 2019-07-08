c6c4fd1169087b4599c46aca869d55dfcbd78f7c
William Collins <wcollins185@gmail.com>
2019-07-08 01:21:14 -0700

    began coding the choice delete function

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,14 +1,22 @@
-let choiceArr = ["burger King", "McDonalds", "Taco Bell"];
-
+// Create an Array to take in values
+let choiceArr = [];
+// Event listener for adding values to the array and adding values to Choices Menu
 function pickOne() {
     let newChoice = document.getElementById("inputField").value;
     choiceArr.push(newChoice);
 
     let newVal = "";
     for (let i = 0; i < choiceArr.length; i++) {
-        newVal = newVal + choiceArr[i] + "<br/>";
+        newVal = newVal + choiceArr[i] + "<br/> <button/>";
     }
-        document.getElementById("outcomeMenu").innerHTML = newVal;
+        document.getElementById("choiceBody").innerHTML = newVal;
+        document.getElementById("inputField").value = "";
 
     console.log(choiceArr);
+}
+
+// Function to remove choices
+function editChoice() {
+    let xChoice = document.getElementById("choiceBody");
+    xChoice.removeChild(xChoice.childNode[0]);
 }
