6c66661f4c715a0c3139ce2852b921aa9fe505b9
William Collins <wcollins185@gmail.com>
2019-07-08 23:50:07 -0700

    saving updates to index.js and index.html

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,5 @@
 // Create an Array to take in values
-let choiceArr = [];
+const choiceArr = [];
 // Event listener for adding values to the array and adding values to Choices Menu
 function pickOne() {
     let newChoice = document.getElementById("inputField").value;
@@ -7,16 +7,23 @@ function pickOne() {
 
     let newVal = "";
     for (let i = 0; i < choiceArr.length; i++) {
-        newVal = newVal + choiceArr[i] + "<br/> <button/>";
+        newVal = newVal + choiceArr[i] + "<br/>";
     }
         document.getElementById("choiceBody").innerHTML = newVal;
         document.getElementById("inputField").value = "";
 
     console.log(choiceArr);
 }
+    document.getElementById("choiceBody").onclick = function() {
+        let xChoice = document.getElementById("choiceBody");
+        xChoice.removeChild(xChoice.childNodes[0]);
+    };
 
-// Function to remove choices
-function editChoice() {
-    let xChoice = document.getElementById("choiceBody");
-    xChoice.removeChild(xChoice.childNode[0]);
+// random choice selection and print selection
+const chosenOne = [];
+
+function randomChoice() {
+    return chosenOne[Math.floor(Math.random()*chosenOne.length)];
 }
+
+console.log(randomChoice(chosenOne));
