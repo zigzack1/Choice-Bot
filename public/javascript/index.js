7b2ca5f5c1f72701a93e9635b78b39182c5348a1
wcollins185 <wcollins185@gmail.com>
2019-07-11 22:20:51 -0700

    saving work

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,29 +1,35 @@
 // Create an Array to take in values
-const choiceArr = [];
+// const choiceArr = [];
 // Event listener for adding values to the array and adding values to Choices Menu
+// function pickOne() {
+//   let newChoice = document.getElementById("inputField").value;
+//   choiceArr.push(newChoice);
+
+//   let newVal = "";
+//   for (let i = 0; i < choiceArr.length; i++) {
+//     newVal = newVal + choiceArr[i] + "<br/>";
+//   }
+//   document.getElementById("choiceBody").innerHTML = newVal;
+//   document.getElementById("inputField").value = "";
+
+//   console.log(choiceArr);
+// }
+// document.getElementById("choiceBody").onclick = function () {
+//   let xChoice = document.getElementById("choiceBody");
+//   xChoice.removeChild(xChoice.childNodes[0]);
+// };
+
+// // random choice selection and print selection
+// const chosenOne = [];
+
+// function randomChoice() {
+//   return chosenOne[Math.floor(Math.random() * chosenOne.length)];
+// }
+
+// console.log(randomChoice(chosenOne));
+
 function pickOne() {
-    let newChoice = document.getElementById("inputField").value;
-    choiceArr.push(newChoice);
-
-    let newVal = "";
-    for (let i = 0; i < choiceArr.length; i++) {
-        newVal = newVal + choiceArr[i] + "<br/>";
-    }
-        document.getElementById("choiceBody").innerHTML = newVal;
-        document.getElementById("inputField").value = "";
-
-    console.log(choiceArr);
+  let newChoices ="";
+  let choiceBank = document.getElementById('choiceBody'); 
+  
 }
-    document.getElementById("choiceBody").onclick = function() {
-        let xChoice = document.getElementById("choiceBody");
-        xChoice.removeChild(xChoice.childNodes[0]);
-    };
-
-// random choice selection and print selection
-const chosenOne = [];
-
-function randomChoice() {
-    return chosenOne[Math.floor(Math.random()*chosenOne.length)];
-}
-
-console.log(randomChoice(chosenOne));
