2eeb0d986894beeea0b0aff989cdf0a84932d307
William Collins <wcollins185@gmail.com>
2019-07-18 16:40:28 -0700

    saving work

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,46 +1,46 @@
-// Create an Array to take in values
-const choiceArr = [];
-// Event listener for adding values to the array and adding values to Choices Menu
-function gotChoices() {
-  let newChoice = document.getElementById("inputField").value;
-  choiceArr.push(newChoice);
+// // Create an Array to take in values
+// const choiceArr = [];
+// // Event listener for adding values to the array and adding values to Choices Menu
+// function gotChoices() {
+//   let newChoice = document.getElementById("inputField").value;
+//   choiceArr.push(newChoice);
 
-  let newVal = "";
-  for (let i = 0; i < choiceArr.length; i++) {
-    newVal = newVal + choiceArr[i] + "br";
-  }
-  document.getElementById("choiceList").innerHTML = newVal;
-  document.getElementById("inputField").value = "";
+//   let newVal = "";
+//   for (let i = 0; i < choiceArr.length; i++) {
+//     newVal = newVal + choiceArr[i] + "<br/>";
+//   }
+  
+//   document.getElementById("choiceList").innerHTML = newVal;
+//   document.getElementById("inputField").value = "";
 
-  console.log(choiceArr);
-}
-document.getElementById("choiceList").onclick = function () {
-  let xChoice = document.getElementById("choiceList");
-  xChoice.removeChild(xChoice.childNodes[0]);
-};
+//   console.log(choiceArr);
+// }
+// document.getElementById("choiceList").onclick = function () {
+//   let xChoice = document.getElementById("choiceList");
+//   xChoice.removeChild(xChoice.childNodes[0]);
+// };
 
-// random choice selection and print selection
-const chosenOne = [];
+// // random choice selection and print selection
+// function pickOne() {
 
-function pickOne() {
-  return chosenOne[Math.floor(Math.random() * chosenOne.length)];
-}
+//   return choiceArr[Math.floor(Math.random() * choiceArr.length)];
+// }
 
-console.log(randomChoice(chosenOne));
+// console.log(pickOne(choiceArr));
 
 
 // // user entry desplays as a button
-// function gotChoices() {
-//     let btn = document.createElement('button');
-//     btn.innerHTML = document.getElementById('inputField').value;
-//     document.getElementById('choiceList').appendChild(btn);
-// }
-// // entrys store as an array when clicked and returns a random selection
-// function pickOne() {
-//     let choiceArr = [];
-//     let choices = document.getElementById('choiceList');
+function gotChoices() {
+    let btn = document.createElement('button');
+    btn.innerHTML = document.getElementById('inputField').value;
+    document.getElementById('choiceList').appendChild(btn, 'br');
+}
+// entrys store as an array when clicked and returns a random selection
+function pickOne() {
+    let choiceArr = [];
+    let choices = document.getElementById('choiceList');
     
-//     choiceArr.push(choices);
-//     console.log (choiceArr);
-// }
+    choiceArr.push(choices);
+    console.log (choiceArr);
+}
  
