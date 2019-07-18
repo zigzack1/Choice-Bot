6797ab15db727a594137a6e09d6323558ae4b7e5
William Collins <wcollins185@gmail.com>
2019-07-17 23:34:20 -0700

    made changes to the index.js file

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,44 +1,46 @@
-// // Create an Array to take in values
-// const choiceArr = [];
-// // Event listener for adding values to the array and adding values to Choices Menu
-// function gotChoices() {
-//   let newChoice = document.getElementById("inputField").value;
-//   choiceArr.push(newChoice);
-
-//   let newVal = "";
-//   for (let i = 0; i < choiceArr.length; i++) {
-//     newVal = newVal + choiceArr[i] + "<button>";
-//   }
-//   document.getElementById("choiceBody").innerHTML = newVal;
-//   document.getElementById("inputField").value = "";
-
-//   console.log(choiceArr);
-// }
-// document.getElementById("choiceBody").onclick = function () {
-//   let xChoice = document.getElementById("choiceBody");
-//   xChoice.removeChild(xChoice.childNodes[0]);
-// };
-
-// // random choice selection and print selection
-// const chosenOne = [];
-
-// function randomChoice() {
-//   return chosenOne[Math.floor(Math.random() * chosenOne.length)];
-// }
-
-// console.log(randomChoice(chosenOne));
-
-
-
+// Create an Array to take in values
+const choiceArr = [];
+// Event listener for adding values to the array and adding values to Choices Menu
 function gotChoices() {
-    let btn = document.createElement('button');
-    btn.innerHTML = document.getElementById('inputField').value
-    document.getElementById('choiceList').appendChild(btn);
+  let newChoice = document.getElementById("inputField").value;
+  choiceArr.push(newChoice);
+
+  let newVal = "";
+  for (let i = 0; i < choiceArr.length; i++) {
+    newVal = newVal + choiceArr[i] + "br";
+  }
+  document.getElementById("choiceList").innerHTML = newVal;
+  document.getElementById("inputField").value = "";
+
+  console.log(choiceArr);
 }
+document.getElementById("choiceList").onclick = function () {
+  let xChoice = document.getElementById("choiceList");
+  xChoice.removeChild(xChoice.childNodes[0]);
+};
 
-
+// random choice selection and print selection
+const chosenOne = [];
 
 function pickOne() {
-    let choiceArr = [];
-    let choiceList = document.getElementById()
+  return chosenOne[Math.floor(Math.random() * chosenOne.length)];
 }
+
+console.log(randomChoice(chosenOne));
+
+
+// // user entry desplays as a button
+// function gotChoices() {
+//     let btn = document.createElement('button');
+//     btn.innerHTML = document.getElementById('inputField').value;
+//     document.getElementById('choiceList').appendChild(btn);
+// }
+// // entrys store as an array when clicked and returns a random selection
+// function pickOne() {
+//     let choiceArr = [];
+//     let choices = document.getElementById('choiceList');
+    
+//     choiceArr.push(choices);
+//     console.log (choiceArr);
+// }
+ 
