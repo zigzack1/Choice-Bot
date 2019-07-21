46fb713310f1f9d6ea89cc00ec5208e4ee10ad59
William Collins <wcollins185@gmail.com>
2019-07-21 14:28:27 -0700

    saving work

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -9,11 +9,20 @@ function gotChoices() {
   for (let i = 0; i < choiceArr.length; i++) {
     newVal = newVal + choiceArr[i] + "<br/>";
   }
-
+  
   document.getElementById("choiceList").innerHTML = newVal;
   document.getElementById("inputField").value = "";
 
   // console.log(choiceArr);
+}
+
+//  Delete choices and update Array
+function removeChoice() {
+  let ammendArr = choiceArr.indexOf(5);
+  if (ammendArr > -1) {
+    choiceArr.splice(index, 1);
+  }
+  console.log(choiceArr);
 }
 // document.getElementById("choiceList").onclick = function () {
 //   let xChoice = document.getElementById("choiceList");
@@ -23,15 +32,13 @@ function gotChoices() {
 // random choice selection and print selection
 function pickOne() {
 
-  if (choiceArr.length > [0]) {
+  if (choiceArr.length > [2]) {
     let randomChoice = choiceArr[Math.floor(Math.random() * choiceArr.length)];
-    document.getElementById('outcomeMenu').innerHTML = randomChoice;
-  } else {
-    document.getElementById('outcomeMenu').innerHTML = " Oops... Looks like you haven't entered any choices."
+    document.getElementById('outcomeMenu').innerHTML = "Choice Bot picked " + randomChoice;
+  }else {
+    document.getElementById('outcomeMenu').innerHTML = " Oops... Looks like you haven't entered enough options."
   }
 
-}
-
-function removeChoice() {
 
 }
+
